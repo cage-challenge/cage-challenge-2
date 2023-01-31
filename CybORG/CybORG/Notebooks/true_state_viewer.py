82672360e0fc192dfe77fcae87590e19800b2e58
ChrisBog-MV <chris.bogdiukiewicz@montvieux.com>
2023-01-31 15:54:59 +0000

    Red MDN training:
    - larger model
    - training with more data
    - added visualisation for spot checking test results over 10 samples
    - test results generation fixed issue using a representation of y_true
    that has been throught the VAE.. using raw Y produces much better
    results!

--- CybORG/CybORG/Notebooks/true_state_viewer.py
+++ CybORG/CybORG/Notebooks/true_state_viewer.py
@@ -108,7 +108,65 @@ def display_tree_pairs(tree_pairs):
 
     prev_button.on_click(on_button_clicked)
     next_button.on_click(on_button_clicked)
+    # update_images(index)
+    display(sidebyside_controls, sidebyside_images)
+
+def display_tree_red_preds(tree_red_preds):
+    length = len(tree_red_preds)
+    print(length)
+    index = 0
+
+    prev_button = widgets.Button(description="<<")
+    next_button = widgets.Button(description=">>")
+    int_slider = widgets.IntSlider(name='Index', min=0, max=length-1, step=1, value=index)
+
+    # output = widgets.Output()
+    sidebyside_controls = widgets.HBox([prev_button, next_button, int_slider])
+    
+    im_pre = widgets.Image(value = tree_red_preds[index][0].g.pipe(), format='png', width=200)
+    im_blue = widgets.Image(value = tree_red_preds[index][1].g.pipe(), format='png', width=200)
+    im_red_true = widgets.Image(value = tree_red_preds[index][2].g.pipe(), format='png', width=200)
+    sidebyside_truth_images = widgets.HBox([im_pre, im_blue, im_red_true])
+
+    im_preds = []
+    for tree_pred in tree_red_preds[index][3]:
+        im_preds.append(widgets.Image(value = tree_pred.g.pipe(), format='png', width=150))
+    sidebyside_pred_images = widgets.HBox(im_preds, overflow_x='auto', overflow='scroll')
+
+    def update_images(index):
+        nonlocal im_pre, im_blue, im_red_true, tree_red_preds, sidebyside_controls, sidebyside_truth_images, sidebyside_pred_images
+        im_pre.value = tree_red_preds[index][0].g.pipe()
+        im_blue.value = tree_red_preds[index][1].g.pipe()
+        im_red_true.value = tree_red_preds[index][2].g.pipe()
+        for i, tree_pred in enumerate(tree_red_preds[index][3]):
+            im_preds[i].value = tree_pred.g.pipe()
+#       display(sidebyside_controls, sidebyside_images)
+    
+    def on_button_clicked(b):
+        nonlocal index, length
+        if b ==prev_button:
+            if index >0:
+                index -= 1
+                update_images(index)
+        else:
+            if index < length-1:
+                index += 1
+                update_images(index)
+        int_slider.value=index
+    #     with output:
+
+
+    def on_value_change(change):
+        nonlocal index
+        if index != change.new:
+            index = change.new
+            update_images(index)
+
+    int_slider.observe(on_value_change, names='value')
+
+    prev_button.on_click(on_button_clicked)
+    next_button.on_click(on_button_clicked)
     
 #     update_images(index)
 
-    display(sidebyside_controls, sidebyside_images)
+    display(sidebyside_controls, sidebyside_truth_images, sidebyside_pred_images)
